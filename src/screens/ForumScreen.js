ab81d973afa33cbd53bccc4a0e3f30240c102946
Qudus Olawale Bashiru <qbashy@gmail.com>
2021-10-22 09:20:50 +0100

    implemented google auth logic

--- src/screens/ForumScreen.js
+++ src/screens/ForumScreen.js
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import Constants from 'expo-constants';
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    ActivityIndicator,
+    Modal,
+} from 'react-native';
 import { firebase } from '../helperFunctions/initializeFirebase';
 import { connect } from 'react-redux';
-import signUpWihthEmail from '../helperFunctions/signUpWithEmail';
-import updateUsername from '../helperFunctions/updateUsername';
-import signOutUser from '../helperFunctions/signOutUser';
 
-function ForumScreen({ user }) {
-    const [authError, setAuthError] = useState('');
-
-    const onPress = () => {
-        // const googleProvider = new firebase.auth.GoogleAuthProvider();
-        // googleProvider.addScope('profile');
-        // googleProvider.addScope('email');
-        // firebase
-        //     .auth()
-        //     .signInWithPopup(googleProvider)
-        //     .then((result) => console.log(result));
-    };
-    // console.log(firebase.auth.)
-
-    return (
-        <View style={{ justifyContent: 'center', flex: 1 }}>
-            <Text>ForumScreen</Text>
-            <Button title="Sign Up" onPress={onPress} />
-        </View>
-    );
+function ForumScreen() {
+    return <View style={{ justifyContent: 'center', flex: 1 }}></View>;
 }
 
 const mapStateToProps = ({ settingsState: { user } }) => ({
